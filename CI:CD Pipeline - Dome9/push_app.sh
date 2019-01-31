6356d27ff33c7ba061e7521854783bd1ff5068e4
Idan Perez <idperez@checkpoint.com>
2019-01-31 13:21:07 -0800

    Improve the bash scripts

--- CI:CD Pipeline - Dome9/push_app.sh
+++ CI:CD Pipeline - Dome9/push_app.sh
@@ -2,6 +2,16 @@
 
 # a 'deployment' script based on S3
 # In real world we would probably have a pipline configured to our git repository
+
+PROFILE=$1
+
 rm -f my-app-cft.zip
-zip -r my-app-cft.zip my-app-cft
-aws s3 cp 'my-app-cft.zip'  s3://reinvent2017sid317/
+pushd my-app-cft
+zip -r ../my-app-cft.zip *
+popd
+
+if [ -z "$PROFILE" ]; then
+    aws s3 cp 'my-app-cft.zip'  s3://idan-dome9-cicd-pipeline/
+else
+    aws s3 cp --profile $PROFILE 'my-app-cft.zip'  s3://idan-dome9-cicd-pipeline/
+fi
--- CI:CD Pipeline - Dome9/push_validation_lambda.sh
+++ CI:CD Pipeline - Dome9/push_validation_lambda.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+PROFILE=$1
+
+
+rm -f codepipeline-lambda.zip
+
 pushd codepipeline-lambda
 zip -r ../codepipeline-lambda.zip * 
 popd
-aws s3 cp 'codepipeline-lambda.zip'  s3://reinvent2017sid317/
+
+if [ -z "$PROFILE" ]; then
+    aws s3 cp 'codepipeline-lambda.zip'  s3://idan-dome9-cicd-pipeline/
+else
+    aws s3 cp --profile $PROFILE 'codepipeline-lambda.zip'  s3://idan-dome9-cicd-pipeline/
+fi
